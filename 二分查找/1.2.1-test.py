fe11f667f6d08a7d36f0c8bbd36daae9a45e1ea1
明乃超 <mingnaichao@gmail.com>
2018-11-04 16:20:10 +0800

    learn

---
+++ 二分查找/1.2.1-test.py
@@ -0,0 +1,11 @@
+from binary_search import binary_search
+
+list = [i for i in range(129)]
+list2 = [i for i in range(259)]
+# print list
+
+# print binary_search(list,111)
+# print binary_search(list,66)
+# print binary_search(list,128)
+
+print binary_search(list2,258)
